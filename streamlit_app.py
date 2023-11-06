30705df9cc947ad553f315f9214e41efae2ee864
Rory-git-hub <57959660+Rory-git-hub@users.noreply.github.com>
2023-11-06 11:56:27 +0000

    Update streamlit_app.py
    Added filter functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado' , 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
